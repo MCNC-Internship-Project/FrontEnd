79bfc8b4747a5f530b85562998f3aabb200f7265
dnjs99wo12 <dnjs99wo12@hs.ac.kr>
2024-11-04 17:04:43 +0900

    refactor : App.vue root-container 추가

--- mcnc_2024/src/App.vue
+++ mcnc_2024/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <login-component />
+    <div id="root-container">
+
+        <login-component />
+    </div>
 </template>
 
 <script setup>
 import loginComponent from './components/login/loginComponent.vue'
-
 
 </script>
 
