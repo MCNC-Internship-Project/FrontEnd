77218450e1020e0a53792ef1073c4a68637ee6b3
반명우 <caadiq@gmail.com>
2024-11-13 10:33:31 +0900

    feat : 모바일 브라우저에서 url 입력 영역 제외를 위한 함수 추가

--- mcnc_2024/src/main.js
+++ mcnc_2024/src/main.js
@@ -1,17 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
-import vuetify from './plugins/vuetify'
+import vuetify from "./plugins/vuetify";
 
-import './assets/css/main.css'
+import "./assets/css/main.css";
+
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener('load', setScreenSize);
+window.addEventListener('resize', setScreenSize);
 
 const app = createApp(App);
 
 app.use(router);
 app.use(vuetify);
-app.component('VueDatePicker', VueDatePicker);
+app.component("VueDatePicker", VueDatePicker);
 app.mount("#app");
