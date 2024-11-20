9ac264bf08cbebbd7512b712d95d89e2942cc509
Wisdomcastle <sgh427@hs.ac.kr>
2024-11-20 09:40:27 +0900

    feat: 목데이터 분리

--- mcnc_2024/src/components/survey-participation/SurveyParticipation.vue
+++ mcnc_2024/src/components/survey-participation/SurveyParticipation.vue
@@ -82,42 +82,9 @@
   <script setup>
   import { ref } from 'vue';
   import router from '@/router';
+  import { mockSurveyData, mockEndDate } from './mockData/mockSurveyData'; // 목데이터 가져오기
   
-  const survey = ref({
-    title: '설문조사 제목',
-    description: '설문지 설명',
-    questions: [
-      {
-        id: 1,
-        title: '질문 1',
-        type: 'obj_radio',
-        options: [
-          { id: 1, label: '옵션 1', value: 'option1' },
-          { id: 2, label: '옵션 2', value: 'option2' },
-          { id: 3, label: '옵션 3', value: 'option3' },
-          { id: 4, label: '옵션 4', value: 'option4' },
-        ]
-      },
-      {
-        id: 2,
-        title: '질문 2',
-        type: 'subj',
-        options: []
-      },
-      {
-        id: 3,
-        title: '질문 3',
-        type: 'obj_check',
-        options: [
-          { id: 1, label: '옵션1', value: 'option_1' },
-          { id: 2, label: '옵션2', value: 'option_2' },
-          { id: 3, label: '옵션3', value: 'option_3' },
-          { id: 4, label: '옵션4', value: 'option_4' },
-        ]
-      }
-    ]
-  });
-  
+  const survey = ref(mockSurveyData); // 목데이터 참조
   const answers = ref({});
   survey.value.questions.forEach(question => {
     if (question.type === 'obj_check') {
@@ -134,7 +101,7 @@
   
   const submitSurvey = () => {
     const currentDate = new Date();
-    const endDate = new Date('2024-11-21'); // 예시로 끝나는 날짜를 설정
+    const endDate = new Date(mockEndDate); 
 
     if (currentDate > endDate) {
       router.push('/survey-expired');
