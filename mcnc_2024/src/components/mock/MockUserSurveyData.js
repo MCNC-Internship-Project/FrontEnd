9bb796a1ecc0fe8936788fae62f4c78a61fbc047
Wisdomcastle <sgh427@hs.ac.kr>
2024-11-27 16:52:33 +0900

    feat: 목데이터 내용 수정

--- mcnc_2024/src/components/mock/MockUserSurveyData.js
+++ mcnc_2024/src/components/mock/MockUserSurveyData.js
@@ -1,32 +1,50 @@
-export const mockUserSurveyData = {
-    id: 1,
-    title: "사용자 만족도 조사",
-    description: "서비스에 대한 만족도를 조사하기 위한 설문입니다.",
-    startDate: "2024-01-01",
-    endDate: "2024-12-31",
-    questions: [
-      {
-        id: 1,
-        title: "서비스에 얼마나 만족하시나요?",
-        type: "obj_radio",
-        options: ["매우 만족", "만족", "보통", "불만족", "매우 불만족"],
-      },
-      {
-        id: 2,
-        title: "서비스 개선을 위해 추가 의견을 남겨주세요.",
-        type: "subj",
-      },
-      {
-        id: 3,
-        title: "서비스에서 가장 만족한 부분을 선택해주세요.",
-        type: "obj_check",
-        options: ["디자인", "사용성", "기능", "고객 지원"],
-      },
-    ],
-    responses: {
-      1: "매우 만족", // 첫 번째 질문 응답
-      2: "더 많은 기능이 있었으면 좋겠어요.", // 두 번째 질문 응답
-      3: ["디자인", "기능"], // 세 번째 질문 응답
+export const surveyData = {
+  surveyId: 46,
+  title: '이건희의 데모 설문',
+  description: '매일 연차쓰고 싶다',
+  createDate: '2024-11-26T13:22:21',
+  expireDate: '2024-12-20T21:00:00',
+  creatorId: 'ghlee000125',
+  questionList: [
+    {
+      quesId: 84,
+      body: '집에 얼마나 가고 싶나요?',
+      questionType: 'OBJ_MULTI', // 객관식(다중 선택)
+      selectionList: [
+        { selectionId: { quesId: 84, sequence: 0 }, body: '진짜 가고 싶음', isEtc: false },
+        { selectionId: { quesId: 84, sequence: 1 }, body: '정말 가고 싶음요', isEtc: false },
+        { selectionId: { quesId: 84, sequence: 2 }, body: '매우 가고 싶음', isEtc: false },
+        { selectionId: { quesId: 84, sequence: 3 }, body: '찐으로 가고 싶음', isEtc: false },
+        { selectionId: { quesId: 84, sequence: 4 }, body: '미치도록 가고 싶음', isEtc: false },
+      ],
+      userResponse: ['진짜 가고 싶음', '매우 가고 싶음'], // 사용자가 선택한 항목
     },
-  };
-  
+    {
+      quesId: 85,
+      body: '이건희의 집은 과연 어디일까요?',
+      questionType: 'OBJ_SINGLE', // 객관식(단일 선택)
+      selectionList: [
+        { selectionId: { quesId: 85, sequence: 0 }, body: '홈플러스 익스프레스 앞', isEtc: false },
+        { selectionId: { quesId: 85, sequence: 1 }, body: '학동역 앞', isEtc: false },
+        { selectionId: { quesId: 85, sequence: 2 }, body: '모빌씨앤씨 지하2층', isEtc: false },
+        { selectionId: { quesId: 85, sequence: 3 }, body: '율암빌딩 7층', isEtc: false },
+        { selectionId: { quesId: 85, sequence: 4 }, body: '자유롭게 맞춰보세요', isEtc: true },
+      ],
+      userResponse: '모빌씨앤씨 지하2층', // 사용자가 선택한 항목
+    },
+    {
+      quesId: 86,
+      body: '집에 가고 싶은 만큼 소리 질러 ~~',
+      questionType: 'SUBJECTIVE', // 주관식
+      selectionList: [],
+      userResponse: '아무리 외쳐도 집에 못 가서 슬픔', // 사용자가 입력한 답변
+    },
+    {
+      quesId: 87,
+      body: '자유롭게 집 가고 싶은 마음을 표현하세요',
+      questionType: 'SUBJECTIVE', // 주관식
+      selectionList: [],
+      userResponse: '오늘 당장 집에 가고 싶어요 ㅠㅠ', // 사용자가 입력한 답변
+    },
+  ],
+};
