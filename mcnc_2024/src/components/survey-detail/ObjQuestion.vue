6efab85102dc16bbdf4648bcd30a70615a6ef300
Kim Eunsu <kimes1405@gmail.com>
2024-11-28 13:42:08 +0900

    feat: 설문 결과 주관식, 객관식 항목 렌더링 구현

---
+++ mcnc_2024/src/components/survey-detail/ObjQuestion.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="question-container">
+        <h3 class="question-title">{{ question.body }}</h3>
+        <div class="chart-wrapper">
+            <canvas ref="barChartCanvas"></canvas>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, defineProps } from "vue";
+import { Chart } from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+Chart.register(ChartDataLabels);
+
+const barChartCanvas = ref(null);
+const props = defineProps({
+    question: Object,
+});
+
+onMounted(() => {
+    if (barChartCanvas.value) {
+        const canvasContext = barChartCanvas.value.getContext("2d");
+        createChart(canvasContext, props.question);
+    }
+});
+
+function createChart(chartElement, question) {
+    const labels = question.selectionList.map((option) => option.body);
+    const data = question.selectionList.map((option) => option.responseCount);
+
+    new Chart(chartElement, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    data: data,
+                    backgroundColor: "#B4DBFF",
+                    borderRadius: 3,
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                datalabels: {
+                    anchor: "center",
+                    align: "center",
+                    formatter: (value) => (value === 0 ? "" : value), 
+                    color: "#555",
+                    font: {
+                        size: 12,
+                    },
+                },
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    grid: { display: false },
+                    ticks: { font: { size: 12 } },
+                },
+                y: {
+                    grid: { display: false },
+                    ticks: {
+                        callback: (value) => (Number.isInteger(value) ? value : ""),
+                        font: { size: 10 },
+                    },
+                },
+            },
+        },
+    });
+}
+</script>
+
+
+<style scoped>
+.question-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #FAF8F8;
+    border: solid 1px #EFF0F6;
+    border-radius: 16px;
+    box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.15);
+    margin-bottom: 16px;
+    padding: 20px 16px 0px 16px;
+}
+
+.question-title {
+    font-size: 1rem;
+    font-weight: bold;
+    color: #8C8C8C;
+    margin-bottom: 12px;
+    background-color: #FFF;
+    padding: 8px 12px;
+    border-radius: 8px;
+    border: 1px solid #EFF0F6;
+}
+
+.chart-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 300px;
+    margin-top: 12px;
+    margin-bottom: 12px;
+}
+canvas{
+    width: 100%;
+    max-width: 600px;
+    height: auto;
+}
+</style>
---
+++ mcnc_2024/src/components/survey-detail/ResultRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <SubjectiveQuestion v-if="question.questionType === 'SUBJECTIVE'" :question="question" />
+      <ObjectiveQuestion v-else :question="question" />
+    </div>
+  </template>
+  
+  <script setup>
+  import SubjectiveQuestion from '@/components/survey-detail/SubjQuestion.vue';
+  import ObjectiveQuestion from '@/components/survey-detail/ObjQuestion.vue';
+  import { defineProps } from 'vue';
+  defineProps({
+    question: Object,
+  });
+  </script>
+  
