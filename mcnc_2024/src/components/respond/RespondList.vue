67fa0b016c4065fc9a390e61c8f32308f521b787
Kim Eunsu <kimes1405@gmail.com>
2024-12-12 10:22:28 +0900

    feat: 참여한 설문 목록 응답날짜, 프로필 추가

--- mcnc_2024/src/components/respond/RespondList.vue
+++ mcnc_2024/src/components/respond/RespondList.vue
@@ -9,7 +9,8 @@
         <div class="list-container">
             <v-infinite-scroll v-if="!noResult" :items="surveyList" :onLoad="load" color="var(--primary)">
                 <template v-for="(item) in surveyList" :key="item">
-                    <SurveyCard :survey="item" @click="goToDetail(item.surveyId)" />
+                    <SurveyCard :survey="item" :respondDate="item.respondDate" :showStatusBadge="false"
+                        :showProfile="true" @click="goToDetail(item.surveyId)" />
                 </template>
                 <template v-slot:empty>
                 </template>
