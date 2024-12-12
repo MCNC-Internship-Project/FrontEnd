3a9b948974499c064b564c3aabc29c649be85d58
Kim Eunsu <kimes1405@gmail.com>
2024-12-12 10:23:05 +0900

    refactor: surveyCard 컴포넌트 리팩토링

--- mcnc_2024/src/components/common/SurveyCard.vue
+++ mcnc_2024/src/components/common/SurveyCard.vue
@@ -2,10 +2,17 @@
     <div class="item-container" v-ripple>
         <div class="item-header-container">
             <div class="item-title">{{ survey.title }}</div>
-            <div class="item-expire" :class="{ 'expired': !survey.expireDateValid }">{{ survey.expireDateValid ? '진행중' : '종료' }}</div>
+            <div v-if="showStatusBadge" class="item-expire" :class="{ 'expired': !survey.expireDateValid }">{{ survey.expireDateValid ? '진행중' :
+                '종료' }}</div>
+            <div v-else-if="showProfile" class="item-footer-container">
+                <img class="item-img-profile" src="@/assets/images/icon_profile_default.svg" alt="profile icon" />
+                <div class="item-userid">{{ survey.creatorId }}</div>
+            </div>
         </div>
         <div class="item-description">{{ survey.description }}</div>
-        <div class="item-date">{{ formatDate(survey.createDate, survey.expireDate) }}</div>
+        <div v-if="createDate && expireDate" class="item-date">{{ formatDate(survey.createDate, survey.expireDate) }}
+        </div>
+        <div v-else-if="respondDate" class="item-date">{{ formatDate(survey.respondDate) }}</div>
     </div>
 </template>
 
@@ -17,10 +24,31 @@ defineProps({
     survey: {
         type: Object,
     },
+    respondDate: {
+        type: String,
+    },
+    createDate: {
+        type: String,
+    },
+    expireDate: {
+        type: String,
+    },
+    showStatusBadge: {
+        type: Boolean,
+        default: false
+    },
+    showProfile: {
+        type: Boolean,
+        default: false
+    }
 });
 
-const formatDate = (createDate, expireDate) => {
-    return dayjs(createDate).format('YYYY.MM.DD') + ' ~ ' + dayjs(expireDate).format('YYYY.MM.DD');
+const formatDate = (date1, date2 = null) => {
+    if (date2) {
+        return dayjs(date1).format('YYYY.MM.DD') + ' ~ ' + dayjs(date2).format('YYYY.MM.DD');
+    } else {
+        return dayjs(date1).format('YYYY.MM.DD');
+    }
 }
 </script>
 
@@ -86,5 +114,28 @@ const formatDate = (createDate, expireDate) => {
     margin-top: auto;
     font-size: 0.875rem;
     color: #8D8D8D;
+}
+
+
+.item-footer-container {
+    display: flex;
+    align-items: center;
+    margin-top: auto;
+}
+
+.item-userid {
+    margin-left: 8px;
+    font-size: 0.875rem;
+    color: #919191;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-word;
+    white-space: nowrap;
+}
+
+.item-img-profile {
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
 }
 </style>
