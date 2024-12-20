91285eb0b866be2e8c1a757edd52d58d4f40e1c2
Wisdomcastle <sgh427@hs.ac.kr>
2024-12-20 13:07:29 +0900

    style: 주석 변경경

--- mcnc_2024/src/components/form/SurveySubmit.vue
+++ mcnc_2024/src/components/form/SurveySubmit.vue
@@ -3,7 +3,6 @@
 
     <img src="@/assets/images/icon_complete.svg" alt="아이콘" class="survey-icon" />
 
-      <!-- 참여 완료 메시지 -->
       <p class="completion-message">응답이 제출되었습니다!</p>
       <p class="p1">설문에 참여해 주셔서 감사합니다.</p>
 
@@ -17,44 +16,44 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 const goBackToHome = () => {
-  router.replace('/');  // 홈 페이지로 돌아가기
+  router.replace('/');  
 };
 </script>
 
 <style scoped>
 .survey-completion {
   display: flex;              
-  flex-direction: column;       /* 세로 방향으로 정렬 */
-  align-items: center;          /* 가로 중심 정렬 */
-  justify-content: center;      /* 세로 중심 정렬 */
-  min-height: 100vh;            /* 화면 전체 높이를 차지 */
+  flex-direction: column;       
+  align-items: center;          
+  justify-content: center;      
+  min-height: 100vh;            
   padding: 0 24px;
-  box-sizing: border-box;       /* 패딩 포함한 크기 계산 */
+  box-sizing: border-box;       
   text-align: center;
 }
 
 img {
-  width: 134px;                 /* 고정 너비 */
-  height: 118px;                /* 고정 높이 */
-  margin-top: 0.5em;             /* 아이콘과 메시지 간 간격 */
+  width: 134px;                 
+  height: 118px;                
+  margin-top: 0.5em;           
 }
 
 .completion-message {
   font-weight: bold;
   color: #374957;
-  margin-top: 20px;             /* 아이콘 아래 고정된 간격 */
+  margin-top: 20px;             
 }
 
 .p1 {
   font-weight: bold;
-  margin-top: 10px;             /* 메시지 간 간격 */
+  margin-top: 10px;             
 }
 
 button {
   background-color: #7796E8;
   color: white;
   padding: 10px 20px;
-  margin-top: 8.5em;             /* 메시지와 버튼 사이 고정 간격 */
+  margin-top: 8.5em;             
   border: none;
   border-radius: 15px;
   cursor: pointer;
